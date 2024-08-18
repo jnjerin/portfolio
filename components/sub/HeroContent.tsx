afee09604f00cb80438dfc7a1cda121d576de504
jnjerin <njerijoan24@gmail.com>
2024-08-18 23:40:33 +0300

    updated styling for hero content

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -3,33 +3,37 @@
 import React from 'react'
 import Image from 'next/image'
 import {motion} from 'framer-motion'
-import {sparklesIcon} from 
 import { slideInFromLeft, slideInFromRight, slideInFromTop } from '@/utils/motion'
-
+import { SparklesIcon } from '@heroicons/react/24/solid'
 
 const HeroContent = () => {
   return (
     <motion.div
     initial='hidden'
     animate='visible'
-    className='flex flex-row items-center px-20 mt-40 w-full z-[20]' 
+    className='flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]' 
     >
         <div  className='h-full w-full flex flex-col gap-5 justify-center m-auto text-start '>
             <motion.div
             variants={slideInFromTop}
-            className='welcome-box py-[8px] px-[4px] border border-[#7042f88b] opacity-[0.9]'
+            className='Welcome-box flex py-[8px] px-[9px] border border-[#7042f88b] opacity-[5]'
             >
-                <sparklesIcon className='text-[#b49bff] mr-[] h-5 w-5' />
-                <h1 className='welcome-text text-[13px]'>Fullstack Developer Portfolio</h1>
+                <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5' />
+                <h1 className='Welcome-text text-[13px]'>
+                    Fullstack Developer Portfolio
+                </h1>
             </motion.div>
 
             <motion.div
             variants={slideInFromLeft(0.5)}
-            className='flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto'
+            className='flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto'
             >
-                providing
-                <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'>the best</span>
-                project experience
+                <span>
+                    Providing 
+                    <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'> the best </span>
+                    project experience
+                </span>
+
             </motion.div>
 
             <motion.p

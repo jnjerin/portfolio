d1f5735a58bb8ec7dde2790fdccc96a26f298fb2
jnjerin <njerijoan24@gmail.com>
2024-08-18 23:55:52 +0300

    create star background component

---
+++ components/sub/StarBackground.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { useState, useRef, Suspense } from "react"
+import { Canvas, useFrame } from "@react-three/fiber"
+import { Points, PointMaterial, Preload } from "@react-three/drei"
+// @ts-ignore
+import * as random from 'maath/random/dist/maath-random-esm'
+
+function StarBackground() {
+  return (
+    <div>StarBackground</div>
+  )
+}
+
+export default StarBackground

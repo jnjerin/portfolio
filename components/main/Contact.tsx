4b3294be2cd66c512b8575df15d47d5596f7e4f8
jnjerin <njerijoan24@gmail.com>
2024-10-13 00:03:22 +0300

    solve linting errors with unescaped character

--- components/main/Contact.tsx
+++ components/main/Contact.tsx
@@ -10,7 +10,7 @@ const Contact = () => {
         className='flex flex-col items-center justify-center'
       >
         <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
-          Let's Connect
+          Let&apos;s connect
         </h1>
       </motion.div>
       <motion.div

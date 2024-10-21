a12ed02ba7683485503605557be5e63125a3ef25
jnjerin <njerijoan24@gmail.com>
2024-10-21 23:58:38 +0300

    Changed projects to grid layout and add responsiveness

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -10,7 +10,8 @@ const Projects = () => {
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
         My Projects
       </h1>
-      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
+      {/* <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10"> */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-4 md:px-10">  
         <ProjectCard
           src="/SpaceWebsite.png" 
           title="Modern Next.js Portfolio" 
